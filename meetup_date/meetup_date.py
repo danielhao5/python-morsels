113373218cad8941cd722f862149f52de45bff8d
pmayd <michael.aydinbas@gmail.com>
2020-01-29 20:54:55 +0100

    solution for meetup_date

--- meetup_date/meetup_date.py
+++ meetup_date/meetup_date.py
@@ -1,28 +1,34 @@
 import datetime
 import calendar
+from enum import IntEnum, unique
+
+@unique
+class Weekday(IntEnum):
+    MONDAY = 0
+    TUESDAY = 1
+    WEDNESDAY = 2
+    THURSDAY = 3
+    FRIDAY = 4
+    SATURDAY = 5
+    SUNDAY = 6
 
 
-def meetup_date(year: int, month: int, nth: int=4, weekday: int=3) -> datetime.date:
-    """Determine which day of the month the San Diego Python meetup should be
+def meetup_date(
+    year: int, month: int, nth: int = 4, weekday: int = Weekday.THURSDAY
+) -> datetime.date:
+    """Determine which day of the month the San Diego Python meetup should be.
 
     The meetup is on the fourth Thrusday of the month
     """
-    date_first_week = datetime.date(year, month, 1)
+    all_weeks_per_month = calendar.monthcalendar(year, month)
+    weeks_with_day = [w for w in all_weeks_per_month if w[weekday] != 0]
+    week_index_with_weekday = nth - 1 if nth >= 0 else nth
 
-    day_delta = (weekday + 1) - date_first_week.isocalendar()[2] # weekday is datetime.date.weekday format not isocalendar, so +1 to be in range 1..7
-    if nth > 0:
-        week_delta = nth - 1 if day_delta >= 0 else nth
-    else:
-        nth_from_end = len(calendar.monthcalendar(year, month)) + nth
-        week_delta = nth_from_end - 1 if day_delta >= 0 else nth_from_end
-
-    three_week_delta = datetime.timedelta(days=day_delta, weeks=week_delta)
-
-    date_fourth_week = date_first_week + three_week_delta
-
-    return date_fourth_week
+    return datetime.date(
+        year, month, weeks_with_day[week_index_with_weekday][weekday]
+    )
 
 
 if __name__ == "__main__":
-    print(meetup_date(2016, 2, nth=4, weekday=3))
+    print(meetup_date(2016, 2, nth=4, weekday=Weekday.THURSDAY))
 
